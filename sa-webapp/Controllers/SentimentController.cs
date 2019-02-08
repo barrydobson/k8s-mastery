f6ea640f9626339affc17eef5ee3804666064b47
Barry Dobson <github@myvoodoo.co.uk>
2019-02-08 13:58:50 +0000

    Create .net core webapp

---
+++ sa-webapp/Controllers/SentimentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sa_webapp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SentimentController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SentimentController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] string value)
+        {
+            var client = _httpClientFactory.CreateClient("sa-logic");
+            string result = await client.GetStringAsync("");
+
+        }
+    }
+}
