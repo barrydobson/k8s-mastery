59d6c588379366925fc5999c75f4112ac7337c4e
Barry Dobson <Barry.Dobson@kpmg.co.uk>
2019-02-08 14:32:43 +0000

    Fix project

---
+++ sa-webapp/sa-webapp/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sa_webapp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ValuesController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] string value)
+        {
+            var client = _httpClientFactory.CreateClient("sa-logic");
+            var result = await client.GetStringAsync("/analyse/sentiment");
+            return Ok(result);
+        }
+    }
+}
---
+++ sa-webapp/sa-webapp/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace sa_webapp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var logicUrl = Configuration.GetValue<string>("SA_LOGIC_URL");
+
+            services.AddHttpClient("sa-logic", c =>
+            {
+                c.BaseAddress = new Uri(logicUrl);
+                c.DefaultRequestHeaders.Add("Accept", "application/json");
+                c.DefaultRequestHeaders.Add("User-Agent", "sa-webapp");
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
