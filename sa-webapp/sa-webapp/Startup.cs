02be81a511ce2c54acd887d72803e48cd7d504a2
Barry Dobson <Barry.Dobson@kpmg.co.uk>
2019-02-12 12:10:45 +0000

    Add Cors

--- sa-webapp/sa-webapp/Startup.cs
+++ sa-webapp/sa-webapp/Startup.cs
@@ -20,6 +20,20 @@ namespace sa_webapp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("app-cors-policy",
+                    builder =>
+                    {
+                        builder
+                            .AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .AllowCredentials();
+                    });
+
+            });
+
             services
                 .AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
@@ -43,6 +57,7 @@ namespace sa_webapp
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors("app-cors-policy");
             app.UseMvc();
         }
     }
